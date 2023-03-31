e224d962e52afba8f35f57dffa63ebba725c155e
chatrapathi-002 <chatrapathirobert@gmail.com>
2023-03-31 12:11:09 +0530

    Show Loading Skeleton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -31,7 +31,7 @@ const GameCard = ({ game }: Props) => {
     web: BsGlobe,
   };
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width="300px" borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
