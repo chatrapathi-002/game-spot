a6a5202aeefaf9e7fc0e57935c998bc204afe2e9
chatrapathi-002 <chatrapathirobert@gmail.com>
2023-04-02 12:51:53 +0530

    final

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Heading, HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import logo from "../assets/logo_course.webp";
 import SearchInput, { Props } from "./SearchInput";
@@ -6,7 +6,16 @@ import SearchInput, { Props } from "./SearchInput";
 const Navbar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Heading
+        backgroundColor="#5752DA"
+        paddingY={2}
+        paddingX={4}
+        borderRadius={8}
+        fontSize="2xl"
+        color="#FFFFFF"
+      >
+        GameSpot
+      </Heading>
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
